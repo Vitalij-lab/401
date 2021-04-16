4c03033e26777d90f960cea3a8745803ecfe19e8
Виталий <vitol1966@mail.ru>
2021-04-16 21:01:58 +0300

    Решение квадратного уровнения

--- js/script.js
+++ js/script.js
@@ -1,47 +1,53 @@
-// ЗАДАЧА № 2
-let a = +prompt("Введите длину первого основания");
-if (a.toString() !== "NaN") {
-   let b = +prompt("Введите длину второго основания");
-   if (b.toString() !== "NaN") {
-      let h = +prompt("Введите высоту");
-      if (h.toString() !== "NaN") {
-         let summ = a + b;
-         alert('Площадь трапеции будет равна ' + summ / 2 * h);
+// Решение квадратного уровнения
+
+let a = +prompt("Введите коэффициент 'а'\nОн не должен = 0!");
+
+if (a > 0 || a < 0){
+
+   let b = +prompt("Введите коэффициент 'b'");
+   
+      if (b > 0 || b < 0 || b == 0) {
+
+         let c = +prompt("Введите коэффициент 'c'");
+      
+            if (c > 0 || c < 0 || c == 0) {
+               
+               let discriminant;
+               discriminant = (b*b - 4*a*c);
+               alert("Дискриминант = " +discriminant);
+               
+                  if (discriminant < 0) {
+                     alert("Уровнение решения не имеет.");
+                  }
+
+                  else if (discriminant == 0) {
+                     let root;
+                     root = ((-b + Math.sqrt(discriminant))/(2*a));
+                     alert(`Уровнение имеет одно решение: ${root}`);
+
+                  }
+                  
+                  else {
+                     let root1;
+                     root1 = ((-b + Math.sqrt(discriminant))/(2*a));
+                     let root2;
+                     root2 = ((-b - Math.sqrt(discriminant))/(2*a));
+                     alert(`Уровнение имеет два решения: ${root1} и ${root2}`);
+                  }
+            }
+
+         else {
+         alert("Ошибка!");
+        }
+
       }
-      else {
-         alert("Вы ввели не корректные данные! Переходим к следующей задаче!");
-      }
-   }
    else {
-      alert("Вы ввели не корректные данные! Переходим к следующей задаче!");
-   }
-}
-else {
-   alert("Вы ввели не корректные данные! Переходим к следующей задаче!");
+       alert("Ошибка!");
+    }
 }
 
+ else {
+       alert("Ошибка!");
+    }
 
 
-//  ЗАДАЧА № 3
-let summ = +prompt("Введите сумму вклада");
-let interest = +prompt("Введите проценты");
-let year = +prompt("Введите количество лет");
-let contribution = summ;
-let interestYear;
-for (let i = 1; i <= year; i++) {
-   interestYear = contribution / 100 * interest;
-   contribution = contribution + interestYear;
-
-   alert('Через ' + i + ' лет Ваш вклад будет ' + contribution.toFixed(2) + ' рублей');
-
-}
-alert('Следущая задача');
-// ЗАДАЧА № 4
-
-let p = +prompt("Введите число");
-if (p == 10) {
-   alert("Верно!");
-}
-else {
-   alert("Неверно!");
-}
